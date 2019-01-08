ce4e0d0f244775fdf0b09e6e4127c366f29e39c1
zhammer <zachary_hammer@alumni.brown.edu>
2019-01-07 23:55:52 -0500

    Include backwards file in db parse

--- scripts/parse_zone_csv.py
+++ scripts/parse_zone_csv.py
@@ -1,4 +1,5 @@
 import csv
+import os.path
 import sys
 from pprint import pprint
 from typing import Dict, Iterable, MutableMapping
@@ -9,19 +10,33 @@ from iana_tz_coord import Coordinates
 
 
 ZONE_CSV_FIELDNAMES = ('codes', 'coordinates', 'TZ', 'comments')
-ZoneCsvRow = MutableMapping[str, str]
+CsvRow = MutableMapping[str, str]
 
+BACKWARD_CSV_FIELDNAMES = ('link', 'target', 'link_name')
+
+# i miss messy files
 
 def main() -> None:
-    zone_csv_path = sys.argv[1]
+    iana_tz_dir = sys.argv[1]
+    zone_file_path = os.path.join(iana_tz_dir, 'zone1970.tab')
+    backward_file_path = os.path.join(iana_tz_dir, 'backward')
 
-    with open(zone_csv_path) as zone_csv:
-        rows = csv.DictReader(
-            without_comment_lines(zone_csv),
+    with open(zone_file_path) as zone_file:
+        zone_file_rows = list(csv.DictReader(
+            without_comment_lines(zone_file),
             fieldnames=ZONE_CSV_FIELDNAMES,
             delimiter='\t'
-        )
-        coordinate_pair_by_tz_name = pluck_coordinate_pair_by_tz_name(rows)
+        ))
+        coordinate_pair_by_tz_name = pluck_coordinate_pair_by_tz_name(zone_file_rows)
+
+    with open(backward_file_path) as backward_file:
+        lines = (line.split() for line in backward_file
+                 if not line == '\n' and not line.startswith('#'))
+        current_name_with_backwards_name = [(current, backwards) for the_word_link, current, backwards in lines]
+
+    for current, backwards in current_name_with_backwards_name:
+        if current in coordinate_pair_by_tz_name:
+            coordinate_pair_by_tz_name[backwards] = coordinate_pair_by_tz_name[current]
 
     pprint(coordinate_pair_by_tz_name)
 
@@ -30,7 +45,7 @@ def without_comment_lines(lines: Iterable[str]) -> Iterable[str]:
     return (line for line in lines if not line.startswith('#'))
 
 
-def pluck_coordinate_pair_by_tz_name(zone_csv_rows: Iterable[ZoneCsvRow]) -> Dict[str, Coordinates]:
+def pluck_coordinate_pair_by_tz_name(zone_csv_rows: Iterable[CsvRow]) -> Dict[str, Coordinates]:
     return {zone_csv_row['TZ']: pluck_coordinate_pair(zone_csv_row['coordinates'])
             for zone_csv_row in zone_csv_rows}
 
